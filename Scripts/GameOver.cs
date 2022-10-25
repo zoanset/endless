ecf2ac1f08571a30cd1d614783668c540870fa39
Ismail YILDIZ <65915571+zoanset@users.noreply.github.com>
2022-10-25 17:28:35 +0300

    Endless

---
+++ Scripts/GameOver.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public GameObject gameOverPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            gameOverPanel.SetActive(true);
+        }
+
+    }
+}
